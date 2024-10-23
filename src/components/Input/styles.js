489f01973863902ca901807a3c46a7d2599388b5
unknown <viniciusfa.delima@gmail.com>
2024-10-22 23:24:41 -0300

    first commit

---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const InputContainer = styled.div`
+
+    border: 1px solid #FAFAFA;
+    border-radius: 20px;
+
+    height: 62px;
+    width: 80%;
+
+
+    input{
+    background: transparent;
+    border: 0;
+    width: 98%;
+    height: 62px;
+    padding: 0 20xp;
+    color: #FFF;
+    font-size: 20px;
+    }
+
+`;
+
+export default InputContainer;
---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+
+
+const ItemContainer = styled.div`
+
+    width: 80%;
+
+    h3{
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+
+    p{
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottom: 20px;
+    }
+
+   a.remover {
+        color: #FFF000;
+        margin-top: 10px;
+    }
+
+    hr{
+        color: #FAFAFA60;
+        margin: 20px 0 0 0;
+    }
+
+`;
+
+
+export default ItemContainer;
